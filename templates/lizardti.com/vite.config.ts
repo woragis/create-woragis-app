5a88442d9af9b6d4ab385fe41028e000d63a2f41
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-18 20:50:07 -0300

    Update path @ config

--- templates/lizardti.com/vite.config.ts
+++ templates/lizardti.com/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
