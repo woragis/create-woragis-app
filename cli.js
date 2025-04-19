60685d6eddf19922f13ac25a74fa28907631b1cf
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-18 22:48:46 -0300

    Upgrade project choice

---
+++ cli.js
@@ -0,0 +1,61 @@
+const inquirer = require('@inquirer/prompts')
+
+async function main() {
+  const { input, select, confirm } = inquirer
+  const projectName = await input({ message: 'Project Name:' })
+  const option = await select({
+    message: 'Select a template',
+    choices: [
+      {
+        name: 'React',
+        value: 'react-tanstack',
+        description: 'Default React Template',
+      },
+      {
+        name: 'Next',
+        value: 'next-tanstack',
+        description: 'Default Next Template',
+      },
+      {
+        name: 'React Redux',
+        value: 'react-redux',
+        description: 'React Redux Template',
+      },
+      {
+        name: 'Next Redux',
+        value: 'next-redux',
+        description: 'Next Redux Template',
+      },
+      {
+        name: 'Svelte',
+        value: 'svelte',
+        description: 'Default Svelte Template',
+      },
+      {
+        name: 'Thales',
+        value: 'thales',
+        description: 'Default Thales MVVM Template',
+      },
+      {
+        name: 'LizardTi Aceite',
+        value: 'lizardti-aceite',
+        description: 'Default LizardTi Aceite System Template',
+      },
+    ],
+  })
+  console.log('selected: ', option)
+  const userChoice = option
+  const confirms = await confirm({
+    message: 'Do you want the: ' + userChoice + ' template?',
+  })
+  if (!confirms) {
+    console.log('Exiting...')
+    return
+  } else {
+    console.log(
+      'Creating ' + projectName + ' with ' + userChoice + ' template...'
+    )
+  }
+}
+
+main()
