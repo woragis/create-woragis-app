4a5de3aa79e833c97822663d1f2fbb5e454118a8
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-18 22:01:11 -0300

    Update user type in store

--- templates/thales/src/store/user/types.ts
+++ templates/thales/src/store/user/types.ts
@@ -1,14 +1,22 @@
-export interface UserTokens {
-  token: string
-  refreshToken: string
+export interface Tokens {
+  access: string
+  access_expires: string
+  refresh: string
+  refresh_expires: string
 }
 
 export interface User {
-  id: string
+  birth_date: string | null
+  company: string[]
   email: string
-  name: string
-  status: string
-  tokens: UserTokens
+  first_name: string
+  group: number
+  id: string
+  last_login_at: string
+  last_name: string
+  picture: string | null
+  sex: string | null
+  tokens: Tokens
 }
 
 export interface UserState {
