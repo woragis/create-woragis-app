58f604ea5defecc457d6655eddfc7504b6be57c0
woragis <masteringthecode.woragis+github@gmail.com>
2025-04-18 21:14:59 -0300

    Update store usage in login model

--- templates/lizardti.com/src/pages/Login/model.tsx
+++ templates/lizardti.com/src/pages/Login/model.tsx
@@ -2,10 +2,10 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { Auth } from '@/services/auth'
-import { useLoadingStore } from '../../store/loadingStore'
-import { useToastStore } from '../../store/toastStore'
-import { useNavigate } from 'react-router-dom'
-import { useAuthStore } from '../../store/userStore'
+import { useNavigate } from '@tanstack/react-router'
+import { setLoading } from '@/store/loading/actions'
+import { showToast } from '@/store/toast/actions'
+import { setUser } from '@/store/user/actions'
 
 const loginSchema = z.object({
   email: z.string().email('Email inválido').min(1, 'Email é obrigatório'),
@@ -15,10 +15,6 @@ const loginSchema = z.object({
 type LoginFormData = z.infer<typeof loginSchema>
 
 export const useLogin = () => {
-  const { dispatch: dispatchLoading } = useLoadingStore()
-  const { dispatch: dispatchToast } = useToastStore()
-  const { dispatch: dispatchUser } = useAuthStore()
-
   const navigate = useNavigate()
 
   const {
@@ -31,16 +27,16 @@ export const useLogin = () => {
   })
 
   const onSubmit = async (data: LoginFormData) => {
-    dispatchLoading.setLoading(true)
+    setLoading(true)
     const response = await Auth.loginUser(data)
-    dispatchLoading.setLoading(false)
+    setLoading(false)
 
     if (response.status === 200) {
-      dispatchToast.setOpenToast('success', 'Login efetuado com sucesso')
-      navigate('/inicial-page')
-      dispatchUser.setUser(response.data)
+      showToast('success', 'Login efetuado com sucesso')
+      navigate({ to: '/inicial-page' })
+      setUser(response.data)
     } else {
-      dispatchToast.setOpenToast('error', response.message)
+      showToast('error', response.message)
     }
 
     console.log(data)
