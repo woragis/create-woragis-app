96c432e10e90bb5637a9a851ef7477032fb4e29a
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-03 15:50:59 -0300

    Update extras for them to update on user defined variables

--- extras/react/infra/terraform/variables.tf
+++ extras/react/infra/terraform/variables.tf
@@ -1,33 +1,37 @@
 variable "project_name" {
   description = "Name of the App"
   type        = string
+  default     = "__projectName__"
 }
 
 variable "project_type" {
   description = "Type of the App"
   type        = string
+  default     = "__projectType__"
 }
 
 variable "aws_region" {
   description = "AWS region for deployment"
   type        = string
-  default     = "us-east-1"
+  default     = "__awsRegion__"
 }
 
 variable "domain_name" {
   description = "Domain for the React app"
   type        = string
+  default     = "__domainName__"
 }
 
 variable "subdomain" {
   description = "Subdomain for the React app"
   type        = string
-  default     = "www"
+  default     = "__subdomain__"
 }
 
 variable "bucket_name" {
   description = "S3 bucket name for the React build"
   type        = string
+  default     = "__bucketName__"
 }
 
 variable "create_zone" {
